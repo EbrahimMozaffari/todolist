e2aea52da24c2f0b6d8a96eb295fe340faf9d463
Ebrahim Mozaffari <ebrahim.mozafary@gmail.com>
2023-10-23 19:08:33 +0100

    testing validation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,51 @@
 <template>
   <main class="flex items-center justify-center w-full mt-36">
-
+    <div class="createToDoModal fixed w-full h-full top-0 left-0 flex items-center justify-center z-10 " v-if="createModalOpen">
+      <div class="absolute w-full h-full bg-gray-900 opacity-90" ></div>
+  
+      <div class=" max-h-full w-6/12 mx-auto rounded-xl" >
+        <div class="container overflow-hidden md:rounded-xl rounded-xl">
+          <div class="relative w-full  max-h-full">
+              <div class="relative bg-white rounded-xl shadow dark:bg-gray-700 ">
+                  
+                  <div class="p-6 ">
+                    <form data-te-validation-init>
+                      
+                      <div
+                      class="relative mb-3"
+                      data-te-input-wrapper-init
+                      data-te-validate="input"
+                      data-te-validation-ruleset="isRequired">
+                      <input
+                        type="text"
+                        class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                        id="exampleFormAdvancedInput1"
+                        placeholder="Example label" />
+                      <label
+                        for="exampleFormAdvancedInput1"
+                        class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                        >Example label
+                      </label>
+                    </div>
+                    <button
+                      type="button"
+                      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                      data-te-submit-btn-ref>
+                      Validate
+                    </button>
+                        
+                        <!-- <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this TODO?</h3> -->
+                        <button @click="createConfirm" data-modal-hide="popup-modal" type="button" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 green:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                            Create
+                        </button>
+                        <button @click="createModalOpen = !createModalOpen" data-modal-hide="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">cancel</button>
+                      </form>
+                    </div>
+              </div>
+          </div>
+        </div>
+      </div>
+  </div>
     <div class="fixed w-full h-full top-0 left-0 flex items-center justify-center z-10" v-if="isOpen">
       <div class="absolute w-full h-full bg-gray-900 opacity-50" ></div>
   
@@ -57,14 +102,20 @@ import {uuidv4} from "@/helper/helper"
 import store from "@/store";
 import {onMounted, ref, watch, computed} from "vue";
 
+
 const isOpen = ref(false)
+const createModalOpen = ref(false)
 const selectedId = ref('')
-const modalShow = ref(false)
+const createForm = ref(null)
 const toDoList = computed(()=>{
   return store.getters['app/getToDoList']
 })
 const createItem = ()=>{
-  modalShow.value =true
+  createModalOpen.value = true
+}
+const createConfirm = async()=>{
+  console.log("createForm");
+
 }
 const deleteItem = (item) =>{
   selectedId.value = item.id
@@ -95,3 +146,9 @@ const duplicateItem = async(item) =>{
 }
 
 </script>
+<style scoped>
+.createToDoModal .myInput{
+  color:black;
+  border-radius: 10px;
+}
+</style>
